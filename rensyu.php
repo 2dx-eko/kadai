e94856a2d1b8288901a6067ed37145eeea13a590
2dx-eko <tanakakimiaki11@gmail.com>
2020-12-13 11:10:19 +0900

    no message

--- rensyu.php
+++ rensyu.php
@@ -12,7 +12,10 @@
             前前前前前前前
         </p>
         <p>
+<<<<<<< HEAD
         branchbranchbranch branchbranchbranch branchbranchbranch branchbranchbranch
+=======
+>>>>>>> parent of 6b37e25... no message
         </p>
     </div>
 </body>
